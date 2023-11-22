78ba32e7b05ee1cf64aa5dd2753ddbd0ff076225
Leandro Alves <leandro.silva.alves86@gmail.com>
2023-11-22 05:40:11 -0300

    [Helm] Add ajustes no chart confluent/kafka

--- k8s/kafka/confluent/templates/_helpers.tpl
+++ k8s/kafka/confluent/templates/_helpers.tpl
@@ -2,5 +2,9 @@
 Expand the name of the chart.
 */}}
 {{- define "kafka.fullname" -}}
+{{- if .Values.fullname -}}
+{{- .Values.fullname | trunc 63 | trimSuffix "-" -}}
+{{- else -}}
 {{- printf "%s-%s" .Release.Name .Chart.Name | trunc 63 | trimSuffix "-" }}
-{{- end }}
+{{- end -}}
+{{- end -}}
--- scripts/helm.sh
+++ scripts/helm.sh
@@ -6,8 +6,9 @@ if [[ $2 == *"--lb"* ]]; then
 fi
 
 if [[ $1 == *"-i"* ]]; then
-    helm install kafka bitnami/kafka -f k8s/kafka/values.yaml 
-    helm install kakfa-ui kafka-ui/kafka-ui -f k8s/kafka/ui/values.yaml --set service.type=$SERVICE_TYPE
+    # helm install ingress ingress-nginx/ingress-nginx
+    helm install kafka k8s/kafka/confluent -f k8s/kafka/values.yaml
+    helm install kafka-ui kafka-ui/kafka-ui -f k8s/kafka/ui/values.yaml --set service.type=$SERVICE_TYPE
     helm install mongo bitnami/mongodb -f k8s/mongo/values.yaml
     helm install mongo-express k8s/mongo/ui/   --set service.type=$SERVICE_TYPE
     helm install bacen k8s/api-bank -f k8s/bacen.values.yaml --set service.type=$SERVICE_TYPE
@@ -18,8 +19,9 @@ if [[ $1 == *"-i"* ]]; then
 fi
 
 if [[ $1 == *"-x"* ]]; then
+    # helm uninstall ingress
     helm uninstall kafka
-    helm uninstall kakfa-ui
+    helm uninstall kafka-ui
     helm uninstall mongo 
     helm uninstall mongo-express
     helm uninstall bacen
@@ -30,7 +32,9 @@ if [[ $1 == *"-x"* ]]; then
 fi
 
 if [[ $1 == *"-u"* ]]; then
-    if [[ $2 == *"kafka"* ]]; then
+    if [[ $2 == *"ingress"* ]]; then
+        helm upgrade ingress ingress-nginx/ingress-nginx
+    elif [[ $2 == *"kafka"* ]]; then
         helm upgrade kafka -f ./k8s/kafka/values.yaml
     elif [[ $2 == *"kafka-ui"* ]]; then
         helm upgrade kafka-ui -f ./k8s/kafka/ui/values.yaml --set service.type=$SERVICE_TYPE
