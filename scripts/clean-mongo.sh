06e87ac3d8ec1406e7ea822c2576f24d541f5a6c
Leandro Alves <leandro.silva.alves86@gmail.com>
2023-11-23 20:30:26 -0300

    [Docker] Add proxy reverso

--- scripts/clean-mongo.sh
+++ scripts/clean-mongo.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# MONGO_INITDB_ROOT_USERNAME=root
+# MONGO_INITDB_ROOT_PASSWORD=root123Mudar
 
 mongosh -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD  <<EOF
 use bacen
@@ -15,3 +17,4 @@ EOF
 
 
 # docker-compose exec mongo bash -c "/scripts/clean-mongo.sh"
+# kubectl exec --tty -i mongo-mongodb-6bf879d49f-5gz9z --namespace default -- bash 
